736b30fbecb03bb58bc4c28263a10ea06d69c089
Sarp Ulas <sarpulas@gmail.com>
2012-07-20 17:20:14 +0300

    initial file
    AAA

---
+++ aaa.py
@@ -0,0 +1,23 @@
+import getpass
+
+def tool(n):
+
+  namelist = ['osman','mahmut','sarpulas']
+  pwlist = ['namso','tumham','salupras']
+  
+  username = raw_input('Enter username:\n')
+  print "uname ", username;
+  if username in namelist:
+      i = namelist.index(username)
+      pw = getpass.getpass(prompt='Enter password:\n')
+      if pw == pwlist[int(i)]:
+        print('GREAT SUCCESS')
+      else:
+        print('FAIL')
+  else:
+      print('NAME NOT FOUND')
+  
+
+if __name__ == "__main__":
+    import sys
+    tool(1)
