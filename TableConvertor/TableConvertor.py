85fae74c5e938cfa4d9423b87044130853d4caa1
Alp Sayin <alpsayin@gmail.com>
2012-07-26 02:23:05 +1200

    added excel output formatting libraries

--- TableConvertor/TableConvertor.py
+++ TableConvertor/TableConvertor.py
@@ -4,6 +4,12 @@ exports mysql Tables to different types of files
 """
 
 def saveAsTXT(table, path):
+    try:
+        outputFile = open(path, 'w+')
+        
+        outputFile.close()
+    except:
+        print 'Output file could not be opened'
     return
 
 def saveAsXLSX(table, path):
