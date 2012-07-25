265daf342b93039c9e0ea33c9f92a919f7eba935
Alp Sayin <alpsayin@gmail.com>
2012-07-26 03:51:35 +1200

    can create and save xlsx files

--- TableConvertor/TableConvertor.py
+++ TableConvertor/TableConvertor.py
@@ -3,6 +3,11 @@ Table Convertor Module
 exports mysql Tables to different types of files
 """
 
+from openpyxl.workbook import Workbook
+from openpyxl.writer.excel import ExcelWriter
+
+from openpyxl.cell import get_column_letter
+
 def saveAsTXT(table, path):
     try:
         outputFile = open(path, 'w+')
@@ -13,6 +18,23 @@ def saveAsTXT(table, path):
     return
 
 def saveAsXLSX(table, path):
+    wb = Workbook()
+    entrySheet = wb.worksheets[0]
+    entrySheet.title = 'Entries'
+    entrySheet.cell('A1').value = 'Alp'
+    
+    for col_idx in xrange(1, 40):
+        col = get_column_letter(col_idx)
+        for row in xrange(1, 600):
+            entrySheet.cell('%s%s'%(col, row)).value = '%s%s' % (col, row)
+    
+    wb.create_sheet()
+    catSheet = wb.worksheets[1]
+    catSheet.title = 'Categories'
+    catSheet.cell('A1').value = 'Sayin'
+    
+    wb.save(filename = path)
+    
     return
 
 def saveAsPDF(table, path):
@@ -23,3 +45,8 @@ def saveAsXML(table, path):
 
 def saveAsHTML(table, path):
     return
+
+if __name__ == "__main__":
+    print 'hello'
+    saveAsXLSX('alp', 'alp.xlsx')
+    print 'bye bye'
