00a25940f14121e58fb064d017d9bc96073dccac
Alp Sayin <alpsayin@gmail.com>
2012-07-26 02:18:53 +1200

    initialized TableConverter module

---
+++ TableConvertor/TableConvertor.py
@@ -0,0 +1,19 @@
+"""
+Table Convertor Module
+exports mysql Tables to different types of files
+"""
+
+def saveAsTXT(table, path):
+    return
+
+def saveAsXLSX(table, path):
+    return
+
+def saveAsPDF(table, path):
+    return
+
+def saveAsXML(table, path):
+    return
+
+def saveAsHTML(table, path):
+    return
